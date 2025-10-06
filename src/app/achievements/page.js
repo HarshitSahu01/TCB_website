9683508e5266df7f09fe01b41278ef4e12babbc8
soumyyaagrawal <soumyaagrawal2k6@gmail.com>
2025-10-07 01:06:44 +0530

    basic css done

--- src/app/achievements/page.js
+++ src/app/achievements/page.js
@@ -1,4 +1,5 @@
 import { Oxanium, Montserrat } from "next/font/google";
+// import mdi_github from "/mdi_github.svg";
 
 import './achievement.css';
 const oxanium = Oxanium({
@@ -79,7 +80,7 @@ const PlacementCard = ({ data }) => {
         <svg className="social-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"/><rect width="4" height="12" x="2" y="9"/><circle cx="4" cy="4" r="2"/></svg>
     );
     const InstagramIcon = () => (
-        <svg className="social-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><rect width="20" height="20" x="2" y="2" rx="5" ry="5"/><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"/><line x1="17.5" x2="17.5" y1="6.5" y2="6.5"/></svg>
+        <svg className="social-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><rect width="20" height="20" x="2" y="2" rx="5" ry="5"/><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"/><line x1="17.5" x2="17.5" y1="6.5" y2="6.5"/></svg>
     );
     const GithubIcon = () => (
         <svg className="social-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.67c4 0 8-1.5 8-7.75a4.04 4.04 0 0 0-1.25-3.5 4.47 4.47 0 0 0-1.27-1.12c-.22-.22-.49-.37-.77-.47L12 3z"/></svg>
@@ -97,8 +98,11 @@ const PlacementCard = ({ data }) => {
                     <div className="social-icons">
                         <LinkedinIcon />
                         <InstagramIcon />
-                        <GithubIcon />
-                    </div>
+<img 
+                            src="/mdi_github.svg" 
+                            alt="GitHub" 
+                            className="social-icon icongithub" 
+                        /></div>
                     <span className="company-tag">{data.company}</span>
                 </div>
             </div>
